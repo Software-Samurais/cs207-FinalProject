58deed358a98b9ef8ca56a558fc5697a53f6ea2e
erruiz <eruiz@g.harvard.edu>
2019-11-11 16:57:27 -0500

    Erick's implementation of AD
    - Added operator overloading for addition, subtraction, multiplication,
    division, and power
    - Added methods for sine, cosine, tangent, and exponential
    - NOTE: The methods work, but they do not return zero if the result
    falls below machine precision.

--- milestone2/AD.py
+++ milestone2/AD.py
@@ -6,17 +6,65 @@ class AutoDiff():
         self.val = a
         self.der = da
         
-    # TODO: Operator overloading
+    def __repr__(self):
+        return f"Function value: {self.val}, Derivative value: {self.der}"
         
+    def __add__(self, other):
+        try:
+            return AutoDiff(self.val + other.val, self.der + other.der)
+        except AttributeError:
+            return AutoDiff(self.val + other, self.der)
+        
+    def __radd__(self, other):
+        try: 
+            return AutoDiff(self.val + other.val, self.der + other.der)
+        except AttributeError:
+            return AutoDiff(self.val + other, self.der)
+            
+    def __sub__(self, other):
+        try:
+            return AutoDiff(self.val - other.val, self.der - other.der)
+        except AttributeError:
+            return AutoDiff(self.val - other, self.der)
+            
+    def __rsub__(self, other):
+        try:
+            return AutoDiff(self.val - other.val, self.der - other.der)
+        except AttributeError:
+            return AutoDiff(self.val - other, self.der)
+            
+    def __mul__(self, other):
+        try:
+            return AutoDiff(self.val*other.val, self.der*other.val + self.val*other.der)
+        except AttributeError:
+            return AutoDiff(other*self.val, other*self.der)
+            
+    def __rmul__(self, other):
+        try:
+            return AutoDiff(self.val*other.val, self.der*other.val + self.val*other.der)
+        except AttributeError:
+            return AutoDiff(other*self.val, other*self.der)
+        
+    def __truediv__(self, other):
+        try: 
+            return AutoDiff(self.val/other.val, (self.der*other.val - self.val*other.der)/self.der**2)
+        except AttributeError:
+            return AutoDiff(self.val/other, self.der/other)
+            
+    def __pow__(self, n):
+        return AutoDiff(self.val**n, n*self.val**(n-1)*self.der)
+
+# NOTE: The sine and cosine methods work, but they should return zero if the 
+# values get too small. 
 
 def sin(x):
     return AutoDiff(np.sin(x.val), np.cos(x.val)*x.der)
 
 def cos(x):
-    return AutoDiff(np.cos(x.val), np.sec(x.val)**2*x.der)
+    return AutoDiff(np.cos(x.val), -np.sin(x.val)*x.der)
     
 def tan(x):
-    return AutoDiff(np.tan(x.val), -np.sin(x.val)*x.der
+    return AutoDiff(np.tan(x.val), np.cos(x.val)**(-2)*x.der)
     
 def exp(x):
     return AutoDiff(np.exp(x.val), np.exp(x.val)*x.der)
