0d4c05cd1a6162f4dd81715f6ba1663c9d12bf98
erruiz <eruiz@g.harvard.edu>
2019-11-12 11:20:45 -0500

    Edited `test_AD.py`
    Updated tests to reflect new getter methods and private attributes in
    `AD.py`

--- milestone2/test_AD.py
+++ milestone2/test_AD.py
@@ -13,11 +13,11 @@ def test_AD_add():
     u = AD.AutoDiff(3.0, 0.1)
     v = x + u
     
-    assert y.val == 4.0, y.der == 1.0
+    assert y._val == 4.0, y._der == 1.0
     
-    assert z.val == 4.0, z.der == 1.0
+    assert z._val == 4.0, z._der == 1.0
     
-    assert v.val == 4.0, v.der == 1.1
+    assert v._val == 4.0, v._der == 1.1
     
 def test_AD_sub():
 
@@ -28,11 +28,11 @@ def test_AD_sub():
     u = AD.AutoDiff(3.0, 0.1)
     v = x-u
     
-    assert y.val == -2.0, y.der == 1.0
+    assert y._val == -2.0, y._der == 1.0
     
-    assert z.val == 2.0, z.der == 1.0
+    assert z._val == 2.0, z._der == 1.0
     
-    assert v.val == -2.0, v.der == 0.9
+    assert v._val == -2.0, v._der == 0.9
     
 def test_AD_mul():
     
@@ -43,11 +43,11 @@ def test_AD_mul():
     u = AD.AutoDiff(2.0, 0.1)
     v = x*u
     
-    assert y.val == 3.0, y.der == 3.0
+    assert y._val == 3.0, y._der == 3.0
     
-    assert z.val == 3.0, z.der == 3.0
+    assert z._val == 3.0, z._der == 3.0
     
-    assert v.val == 2.0, v.der == 2.1
+    assert v._val == 2.0, v._der == 2.1
     
 def test_AD_div():
 
@@ -57,48 +57,48 @@ def test_AD_div():
     u = AD.AutoDiff(1.0)
     v = u/x
     
-    assert y.val == 1.0, y.der == 1.0/3.0
+    assert y._val == 1.0, y._der == 1.0/3.0
     
-    assert v.val == 1.0/3.0, v.der == 2.0/3.0**2
+    assert v._val == 1.0/3.0, v._der == 2.0/3.0**2
     
 def test_AD_pow():
     
     x = AD.AutoDiff(2.0)
     y = x**2
     
-    assert y.val == 4.0, y.der == 4.0
+    assert y._val == 4.0, y._der == 4.0
     
 def test_AD_check_tol():
     
     x = AD.AutoDiff(np.pi/4)
     y = AD.check_tol(AD.tan(x))
     
-    assert y.val == 1.0, y.der == 2.0
+    assert y._val == 1.0, y._der == 2.0
     
 def test_AD_sin():
     
     x = AD.AutoDiff(np.pi/2)
     y = AD.sin(x)
     
-    assert y.val == 1.0, y.der == 0.0
+    assert y._val == 1.0, y._der == 0.0
     
 def test_AD_cos():
     
     x = AD.AutoDiff(np.pi)
     y = AD.cos(x)
     
-    assert y.val == -1.0, y.der == 0.0
+    assert y._val == -1.0, y._der == 0.0
     
 def test_AD_tan():
     
     x = AD.AutoDiff(np.pi/4)
     y = AD.tan(x)
     
-    assert y.val == 1.0, y.der == 2.0
+    assert y._val == 1.0, y._der == 2.0
     
 def test_AD_exp():
     
     x = AD.AutoDiff(1.0, 2.0)
     y = AD.exp(x)
     
-    assert y.val == np.exp(1.0), y.der == 2.0*np.exp(1.0)
+    assert y._val == np.exp(1.0), y._der == 2.0*np.exp(1.0)
