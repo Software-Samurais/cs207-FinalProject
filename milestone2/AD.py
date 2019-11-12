f246908cd957ff8a4e22a23d668198bc5cd09bf1
erruiz <eruiz@g.harvard.edu>
2019-11-12 11:18:42 -0500

    Edited `AD.py`
    Added getter methods for `_val` and `_der` properties and made these
    private

--- milestone2/AD.py
+++ milestone2/AD.py
@@ -3,77 +3,85 @@ import numpy as np
 class AutoDiff():
     
     def __init__(self, a, da=1.0):
-        self.val = a
-        self.der = da
+        self._val = a
+        self._der = da
+    
+    @property
+    def val(self):
+        return self._val
+        
+    @property
+    def der(self):
+        return self._der
         
     def __repr__(self):
-        return f"Function value: {self.val}, Derivative value: {self.der}"
+        return f"Function value: {self._val}, Derivative value: {self._der}"
         
     def __add__(self, other):
         try:
-            return AutoDiff(self.val + other.val, self.der + other.der)
+            return AutoDiff(self._val + other._val, self._der + other._der)
         except AttributeError:
-            return AutoDiff(self.val + other, self.der)
+            return AutoDiff(self._val + other, self._der)
         
     def __radd__(self, other):
         try: 
-            return AutoDiff(self.val + other.val, self.der + other.der)
+            return AutoDiff(self._val + other._val, self._der + other._der)
         except AttributeError:
-            return AutoDiff(self.val + other, self.der)
+            return AutoDiff(self._val + other, self._der)
             
     def __sub__(self, other):
         try:
-            return AutoDiff(self.val - other.val, self.der - other.der)
+            return AutoDiff(self._val - other._val, self._der - other._der)
         except AttributeError:
-            return AutoDiff(self.val - other, self.der)
+            return AutoDiff(self._val - other, self._der)
             
     def __rsub__(self, other):
         try:
-            return AutoDiff(other.val - self.val, other.der - self.der)
+            return AutoDiff(other._val - self._val, other._der - self._der)
         except AttributeError:
-            return AutoDiff(other - self.val, self.der)
+            return AutoDiff(other - self._val, self._der)
             
     def __mul__(self, other):
         try:
-            return AutoDiff(self.val*other.val, self.der*other.val + self.val*other.der)
+            return AutoDiff(self._val*other._val, self._der*other._val + self._val*other._der)
         except AttributeError:
-            return AutoDiff(other*self.val, other*self.der)
+            return AutoDiff(other*self._val, other*self._der)
             
     def __rmul__(self, other):
         try:
-            return AutoDiff(self.val*other.val, self.der*other.val + self.val*other.der)
+            return AutoDiff(self._val*other._val, self._der*other._val + self._val*other._der)
         except AttributeError:
-            return AutoDiff(other*self.val, other*self.der)
+            return AutoDiff(other*self._val, other*self._der)
         
     def __truediv__(self, other):
         try: 
-            return AutoDiff(self.val/other.val, (self.der*other.val - self.val*other.der)/self.der**2)
+            return AutoDiff(self._val/other._val, (self._der*other._val - self._val*other._der)/self._der**2)
         except AttributeError:
-            return AutoDiff(self.val/other, self.der/other)
+            return AutoDiff(self._val/other, self._der/other)
             
     def __pow__(self, n):
-        return AutoDiff(self.val**n, n*self.val**(n-1)*self.der)
+        return AutoDiff(self._val**n, n*self._val**(n-1)*self._der)
 
 def check_tol(x, tol=1e-8):
     
     # Check function values
-    if abs(x.val - np.round(x.val)) < tol:
-        x.val = np.round(x.val)
+    if abs(x._val - np.round(x._val)) < tol:
+        x._val = np.round(x._val)
         
     # Check derivative values
-    if abs(x.der - np.round(x.der)) < tol:
-        x.der = np.round(x.der)
+    if abs(x._der - np.round(x._der)) < tol:
+        x._der = np.round(x._der)
 
     return x
         
 def sin(x):
-    return check_tol(AutoDiff(np.sin(x.val), np.cos(x.val)*x.der))
+    return check_tol(AutoDiff(np.sin(x._val), np.cos(x._val)*x._der))
 
 def cos(x):
-    return check_tol(AutoDiff(np.cos(x.val), -np.sin(x.val)*x.der))
+    return check_tol(AutoDiff(np.cos(x._val), -np.sin(x._val)*x._der))
     
 def tan(x):
-    return check_tol(AutoDiff(np.tan(x.val), np.cos(x.val)**(-2)*x.der))
+    return check_tol(AutoDiff(np.tan(x._val), np.cos(x._val)**(-2)*x._der))
     
 def exp(x):
-    return check_tol(AutoDiff(np.exp(x.val), np.exp(x.val)*x.der))
+    return check_tol(AutoDiff(np.exp(x._val), np.exp(x._val)*x._der))
